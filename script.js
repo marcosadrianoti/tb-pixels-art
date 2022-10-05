cc2817f171a5c47613d2cfabf1081b384af132b3
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-05 10:06:43 -0300

    projeto pixels-art - Requisito 5

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
-let colorPalette = document.getElementById('color-palette');
+let colorPaletteArray = [];
+if (localStorage.getItem('colorPalette') != null){ // Testa se existe uma paleta de cores armazenada
+  colorPaletteArray = JSON.parse(localStorage.getItem('colorPalette'));
+}
+
+let myColorPalette = document.getElementById('color-palette');
 for (let index = 1; index <= 4; index++) {
   let colorDiv = document.createElement('div');
   colorDiv.classList.add('color');
-  if (index == 1) {
+  if (index == 1) { //Sempre preenche o primeiro com black;
     colorDiv.style.backgroundColor = 'rgb(00, 00, 00)';
-  } else {
+  } else if(localStorage.getItem('colorPalette') == null){ // Se não tem nada armazenado vai gerar novas cores
     while (colorDiv.style.backgroundColor == '') {
-      colorDiv.style.backgroundColor = randomHex();
+      colorDiv.style.backgroundColor = randomHex(); // Atribui uma cor aleatória
     }
+    colorPaletteArray.push(colorDiv.style.backgroundColor); //Grava num array as novas cores
+  }else{ // Usa as cores que, por ventura, já estão armazenadas
+    // console.log(index, colorPaletteArray);
+    colorDiv.style.backgroundColor = colorPaletteArray[index - 2];
   }
-  colorPalette.appendChild(colorDiv);
+  myColorPalette.appendChild(colorDiv);
 }
+// console.log(colorPalette);
+storageColorPalette(colorPaletteArray);
 
 let btnRandom = document.createElement('button');
 btnRandom.id = 'button-random-color';
 btnRandom.innerText = 'Cores aleatórias'
-colorPalette.appendChild(btnRandom);
+myColorPalette.appendChild(btnRandom);
 btnRandom.addEventListener('click', randomColors);
 
 function randomColors() {
   let colorDivs = document.getElementsByClassName('color');
-  // console.log(colorDivs);
+  colorPaletteArray = [];
   for (let index = 1; index <= 3; index++) {
     colorDivs[index].style.backgroundColor = randomHex();
+    colorPaletteArray.push(colorDivs[index].style.backgroundColor); //Grava num array as novas cores
   }
+  // console.log(colorPalette);
+  storageColorPalette(colorPaletteArray);
 }
 
 function randomHex() {
   return '#' + parseInt(Math.random() * 100) + parseInt(Math.random() * 100) + parseInt(Math.random() * 100);
+}
+
+function storageColorPalette(arrayColors) {
+  localStorage.setItem('colorPalette', JSON.stringify(arrayColors));
+  // console.log(arrayColors);
 }
 
   // let listColors = document.querySelectorAll('.color')
